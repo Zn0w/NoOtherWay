9b578f954d585cc713a2ef62cc1a283a26fb2d20
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-03-31 16:46:58 +0300

    started to work on game display

--- src/com/znow/gamecore/Main.java
+++ src/com/znow/gamecore/Main.java
@@ -1,9 +1,13 @@
 package com.znow.gamecore;
 
+import com.znow.gamecore.graphics.Display;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		Display display = new Display();
 	}
 	
 }
---
+++ src/com/znow/gamecore/graphics/Display.java
@@ -0,0 +1,14 @@
+package com.znow.gamecore.graphics;
+
+import javax.swing.*;
+
+public class Display {
+
+	public Display() {
+		JFrame frame = new JFrame("Hello World");
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+};
