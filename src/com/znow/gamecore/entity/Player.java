d7e0957945becfab9b9aac2625866042a26deebf
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-04-04 19:24:47 +0300

    added some tests

--- src/com/znow/gamecore/entity/Player.java
+++ src/com/znow/gamecore/entity/Player.java
@@ -7,6 +7,9 @@ import com.znow.gamecore.input.*;
 public class Player extends Entity
 {
 	
+	private float speed_x = 0.0001f;
+	private float speed_y = 0.0f;
+	
 	public Player(float x, float y, float width, float height)
 	{
 		super(x, y, width, height);
@@ -15,6 +18,12 @@ public class Player extends Entity
 	@Override
 	public void update()
 	{
+		x += speed_x;
+		//y+= speed_y;
+		
+		if (x >= 600 || x <= 0)
+			speed_x *= -1;
+		
 		if (Keyboard.is_key_pressed(KeyEvent.VK_F))
 		{
 			y += 0.01f;
